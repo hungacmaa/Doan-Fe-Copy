27ea5303166e23888b01102c246b6c288b28f4b2
hungnguyen <hungkojno1@gmail.com>
2024-01-12 13:32:37 +0700

    update api host

--- src/components/Login-Register/Login.js
+++ src/components/Login-Register/Login.js
@@ -51,7 +51,7 @@ function Login() {
                                                 password: values.password
                                             }
                                             setLoginError('')
-                                            axios.post("http://34.204.174.17:8080/login", account).then(resp => {
+                                            axios.post("http://3.225.221.144:8080/login", account).then(resp => {
                                                 dispatch(getAccountLogin(resp.data))
                                                 localStorage.setItem('account', JSON.stringify(resp.data));
                                                 Swal.fire({
